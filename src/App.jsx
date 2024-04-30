c0da9d81a80483badbc9df115910142a6f27829d
dotw0xff <dotw0xff@gmail.com>
2024-04-30 12:21:27 +0900

    project-init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Title, Text } from "@mantine/core";
+
+import classes from "./App.module.css";
+
+function App() {
+  return (
+    <div>
+      <Title className={classes.title}>
+        <Text className={classes.text}>Welecome to Mantine</Text>
+      </Title>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import { MantineProvider } from '@mantine/core'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MantineProvider>
+      <App/>
+    </MantineProvider>
+  </React.StrictMode>,
+)
