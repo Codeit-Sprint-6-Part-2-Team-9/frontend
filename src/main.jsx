909718688a94cdc03203922dcb18f8ab53bd73ae
dotw0xff <dotw0xff@gmail.com>
2024-05-01 16:29:06 +0900

    feat: mantine colorscheme is implemented.

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { MantineProvider } from '@mantine/core';
 import App from './App.jsx';
+import theme from './theme.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MantineProvider>
+    <MantineProvider theme={theme}>
       <App />
     </MantineProvider>
   </React.StrictMode>,
---
+++ src/theme.js
@@ -0,0 +1,30 @@
+import { createTheme } from '@mantine/core';
+
+const theme = createTheme({
+    fontFamily: 'Pretendard',
+    colors: {
+        black: [
+            '#02000E',
+            '#181D26',
+        ],
+        yellow: [
+            '#D2C030',
+        ],
+        brand: [
+            '#F96D69',
+            '#FE5493',
+        ],
+        gray: [
+            '#67666E',
+            '#828282',
+            '#8C92AB',
+            '#A3A5A8',
+        ],
+        white: [
+            '#FFFFFF',
+            '#F7F7F8',
+        ],
+    },
+});
+
+export default theme;
