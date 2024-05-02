4d1797538641638477fd4ad2cf1b69b9378296cd
dotw0xff <154291841+dotw0xff@users.noreply.github.com>
2024-05-02 16:23:34 +0900

    📦 Chore:  mantine으로 컬러스킴 작성

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { MantineProvider } from '@mantine/core';
 import App from './App.jsx';
+import theme from './theme.js';
+import '@mantine/core/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MantineProvider>
+    <MantineProvider theme={theme}>
       <App />
     </MantineProvider>
   </React.StrictMode>,
