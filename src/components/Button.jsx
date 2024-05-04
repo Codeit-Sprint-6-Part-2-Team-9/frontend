97d3aacd14bd1c58fb64cf1fc0ab7da7e09930f6
JHmeatschool <ceojr1234@gmail.com>
2024-05-04 17:34:26 +0900

    feat: 버튼마다 별도 컴포넌트 생성

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,9 @@ import CreditIcon from "../assets/creditIcon.png";
 import PlusIcon from "../assets/plusIcon.png";
 import classes from "../components/Buttons.module.css";
 
-function Buttons({ icon, size, color, children }) {
+export default function Test(props) {
+  const { type, icon, color, children } = props;
+
   const icons = {
     chart: <img src={ChartIcon} alt="Chart" />,
     plus: <img src={PlusIcon} alt="Plus" />,
@@ -13,16 +15,68 @@ function Buttons({ icon, size, color, children }) {
 
   const selectedIcon = icons[icon] || null;
 
-  return (
-    <Group>
-      <Button
-        className={`${classes.Buttons} ${classes[size]} ${classes[color]}`}
-        leftSection={selectedIcon}
-      >
+  const BUTTONS = {
+    landing: (
+      <Button className={`${classes[type]} ${classes[color]}`} size="lg">
         {children}
       </Button>
-    </Group>
-  );
-}
+    ),
+    donate: (
+      <Button className={`${classes[type]} ${classes[color]}`} size="lg">
+        {children}
+      </Button>
+    ),
+    vote: (
+      <Group>
+        <Button
+          className={`${classes.Buttons} ${classes[type]} ${classes[color]}`}
+          leftSection={selectedIcon}
+        >
+          {children}
+        </Button>
+      </Group>
+    ),
+    more: (
+      <Button className={`${classes[type]} ${classes[color]}`} size="lg">
+        {children}
+      </Button>
+    ),
+    recharge: (
+      <Group>
+        <Button
+          className={`${classes.Buttons} ${classes[type]} ${classes[color]}`}
+          leftSection={selectedIcon}
+        >
+          {children}
+        </Button>
+      </Group>
+    ),
+    modaldonate: (
+      <Button className={`${classes[type]} ${classes[color]}`} size="lg">
+        {children}
+      </Button>
+    ),
+    modalvote: (
+      <Button className={`${classes[type]} ${classes[color]}`} size="lg">
+        {children}
+      </Button>
+    ),
+    donation: (
+      <Button className={`${classes[type]} ${classes[color]}`} size="lg">
+        {children}
+      </Button>
+    ),
+    add: (
+      <Group>
+        <Button
+          className={`${classes.Buttons} ${classes[type]} ${classes[color]}`}
+          leftSection={selectedIcon}
+        >
+          {children}
+        </Button>
+      </Group>
+    ),
+  };
 
-export default Buttons;
+  return BUTTONS[type];
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,20 +4,20 @@ const HomePage = () => {
   return (
     <div>
       <h1>HomePage</h1>
-      <Buttons icon="chart" size="vote" color="red">
-        투표하기
+      <Buttons type="add" icon="plus" color="red">
+        추가하기
       </Buttons>
       <br />
-      <Buttons size="landing" color="red">
-        지금 시작하기
+      <Buttons type="vote" icon="chart" color="red">
+        차트 투표하기
       </Buttons>
       <br />
-      <Buttons icon="credit" size="recharge" color="red">
+      <Buttons type="recharge" icon="credit" color="red">
         충전하기
       </Buttons>
       <br />
-      <Buttons icon="plus" size="add" color="red">
-        추가하기
+      <Buttons type="landing" color="red">
+        지금 시작하기
       </Buttons>
       <br />
     </div>
