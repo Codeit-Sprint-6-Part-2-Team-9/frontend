b552e25a406e132d5e975540cef852f9293ba27f
JHmeatschool <162143999+JHmeatschool@users.noreply.github.com>
2024-05-06 16:38:00 +0900

    ✨ Feat: 공용 버튼 컴포넌트 #8
    * feat: create button component
    
    * feat: create button component v2
    
    * feat: Icon Button Component
    
    * feat: Combined Icon & Common Button
    
    * feat: 아이콘 이미지 변경
    
    * feat: 버튼마다 별도 컴포넌트 생성
    
    * feat: 버튼 type 마다 고유한 색상 적용
    
    * feat: 버튼 스타일 조정
    
    * fix: 홈페이지 이전 상태 복구
    
    * rename: Buttons로 통일
    
    * chore: 화살표 함수 변경

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,74 @@
+import { Group, Button } from "@mantine/core";
+import ChartIcon from "../assets/chart.svg";
+import CreditIcon from "../assets/credit.svg";
+import PlusIcon from "../assets/plus.svg";
+import classes from "../components/Buttons.module.css";
+
+const Buttons = (props) => {
+  const { type, icon, children } = props;
+
+  const icons = {
+    chart: <img className={classes.chartGap} src={ChartIcon} alt="Chart" />,
+    credit: <img className={classes.creditGap} src={CreditIcon} alt="Credit" />,
+    plus: <img className={classes.plusGap} src={PlusIcon} alt="Plus" />,
+  };
+
+  const selectedIcon = icons[icon] || null;
+
+  const BUTTONS = {
+    landing: (
+      <Button className={`${classes[type]} ${classes.red}`}>{children}</Button>
+    ),
+    donate: (
+      <Button className={`${classes[type]} ${classes.red}`}>{children}</Button>
+    ),
+    vote: (
+      <Group>
+        <Button
+          className={`${classes[type]} ${classes.red}`}
+          leftSection={selectedIcon}
+        >
+          {children}
+        </Button>
+      </Group>
+    ),
+    more: (
+      <Button className={`${classes[type]} ${classes.black}`}>
+        {children}
+      </Button>
+    ),
+    recharge: (
+      <Group>
+        <Button
+          className={`${classes[type]} ${classes.red}`}
+          leftSection={selectedIcon}
+        >
+          {children}
+        </Button>
+      </Group>
+    ),
+    modalDonate: (
+      <Button className={`${classes[type]} ${classes.grey}`}>{children}</Button>
+    ),
+    modalVote: (
+      <Button className={`${classes[type]} ${classes.red}`}>{children}</Button>
+    ),
+    confirm: (
+      <Button className={`${classes[type]} ${classes.red}`}>{children}</Button>
+    ),
+    add: (
+      <Group>
+        <Button
+          className={`${classes[type]} ${classes.red}`}
+          leftSection={selectedIcon}
+        >
+          {children}
+        </Button>
+      </Group>
+    ),
+  };
+
+  return BUTTONS[type];
+};
+
+export default Buttons;
