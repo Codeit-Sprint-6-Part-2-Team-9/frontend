2f611023c0db55b5b5b7012187655c3203f3c72d
dotw0xff <dotw0xff@gmail.com>
2024-05-07 18:58:44 +0900

    chore: 머지 중 유실된 config.js 복원

--- src/api/config.js
+++ src/api/config.js
@@ -2,4 +2,17 @@ const ClientStateKeys = {
     credits: 'credits',
 };
 
-export { ClientStateKeys };
+const envvars = {
+    serverUrl: 'https://fandom-k-api.vercel.app',
+    teamName: '6-9',
+    pageSize: 10,
+};
+
+const serverStateKeys = {
+    femaleChart: 'female',
+    maleChart: 'male',
+    donation: 'donation',
+    idol: 'idol',
+};
+
+export { ClientStateKeys, envvars, serverStateKeys };
