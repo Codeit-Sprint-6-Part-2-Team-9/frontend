e7ee2691ebf6e9e77f4917fc83f04e4127936be4
JHmeatschool <ceojr1234@gmail.com>
2024-05-08 19:28:03 +0900

    feat: !important 추가

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,11 @@
 import MyCreditRechargeButton from "../components/MyCreditRechargeButton";
 
 function HomePage() {
-  const handleButtonClick = () => {
-    console.log("충전하기 버튼이 클릭되었습니다.");
-    // 여기에 버튼을 클릭했을 때 할 작업을 추가하세요.
-  };
   return (
     <div style={{ backgroundColor: "black" }}>
       <h1>Welcome to HomePage</h1>
       <div style={{ marginLeft: "100px" }}>
-        <MyCreditRechargeButton onClick={handleButtonClick} />
+        <MyCreditRechargeButton />
       </div>
       <br />
       <br />
