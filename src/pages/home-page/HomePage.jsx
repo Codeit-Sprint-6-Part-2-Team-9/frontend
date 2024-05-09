065ea9d304eae9759e9a65844f0de7329a15d957
JHmeatschool <ceojr1234@gmail.com>
2024-05-10 00:43:43 +0900

    feat: 지금 시작하기 버튼 추가

--- src/pages/home-page/HomePage.jsx
+++ src/pages/home-page/HomePage.jsx
@@ -1,4 +1,6 @@
+import { Link } from 'react-router-dom';
 import Container from './Container';
+import Buttons from '../../components/Buttons';
 import classes from './HomePage.module.css';
 
 const HomePage = () => {
@@ -6,6 +8,11 @@ const HomePage = () => {
     <Container>
       <section className={classes.intro}>
         <div style={{ color: 'white' }}> 소개 부분 </div>
+        <div className={classes.test}>
+          <Link to="/list">
+            <Buttons type="landing">지금 시작하기</Buttons>
+          </Link>
+        </div>
       </section>
       <section className={classes.description}>
         <div style={{ color: 'white' }}> 설명 부분 </div>
