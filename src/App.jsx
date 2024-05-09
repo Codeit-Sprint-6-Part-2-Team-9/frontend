77b6359b147895f0c181dde64e83fef89fe5b0e0
Daeyang lee <eodid007@gmail.com>
2024-05-09 16:14:07 +0900

    feat: Layout 추가 #37

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import classes from "./App.module.css";
-import HomePage from "./pages/HomePage";
-import List from "./pages/List";
-import MyPage from "./pages/MyPage";
-import NotFound from "./pages/NotFound";
-import Header from "./components/Layout/Header";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import classes from './App.module.css';
+import HomePage from './pages/HomePage';
+import List from './pages/List';
+import MyPage from './pages/MyPage';
+import NotFound from './pages/NotFound';
+import Header from './components/Layout/Header';
+import Layout from './components/Layout/Layout';
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
       <div className={classes.app}>
         <Routes>
           <Route index element={<HomePage />} />
-          <Route path="/list" element={<List />} />
-          <Route path="/mypage" element={<MyPage />} />
+          <Route element={<Layout />}>
+            <Route path="/list" element={<List />} />
+            <Route path="/mypage" element={<MyPage />} />
+          </Route>
           <Route
             path="/*"
             element={<NotFound errorMessage="페이지가 존재하지 않습니다." />}
