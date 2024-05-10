fa2450622f023c94ba36e40be58dc62c84f2059f
JHmeatschool <ceojr1234@gmail.com>
2024-05-10 11:15:30 +0900

    feat: intro 문구 추가

--- src/pages/home-page/HomePage.jsx
+++ src/pages/home-page/HomePage.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import theme from '../../theme';
 import introimg from '../../assets/introimg.svg';
 import Container from './Container';
 import Buttons from '../../components/Buttons';
@@ -8,14 +9,28 @@ const HomePage = () => {
   return (
     <Container>
       <section className={classes.intro}>
-        <div style={{ color: 'white' }}> 소개 부분 </div>
-        <img className={classes.introImg} src={introimg} alt="인트로 이미지" />
+        <div className={classes.introTitle} style={{ color: 'white' }}>
+          <div className={classes.introTitleFont}>
+            내가 좋아하는 아이돌을
+            <br />
+            가장 <span style={{ color: theme.colors.brand[0] }}>쉽게 덕질</span>
+            하는 방법
+          </div>
+        </div>
+        <div className={classes.introImgWrap}>
+          <img
+            className={classes.introImg}
+            src={introimg}
+            alt="인트로 이미지"
+          />
+        </div>
         <div className={classes.landingButton}>
           <Link to="/list">
             <Buttons type="landing">지금 시작하기</Buttons>
           </Link>
         </div>
       </section>
+
       <section className={classes.description}>
         <div style={{ color: 'white' }}> 설명 부분 </div>
       </section>
