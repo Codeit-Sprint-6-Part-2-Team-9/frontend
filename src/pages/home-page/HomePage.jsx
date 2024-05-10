b1131dc2187eca18fba589efd61425cad5c9c579
JHmeatschool <ceojr1234@gmail.com>
2024-05-11 00:04:25 +0900

    feat: 설명 텍스트 추가

--- src/pages/home-page/HomePage.jsx
+++ src/pages/home-page/HomePage.jsx
@@ -2,6 +2,9 @@ import { Link } from 'react-router-dom';
 import theme from '../../theme';
 import logoimg from '../../assets/logo.svg';
 import introimg from '../../assets/introimg.svg';
+import donatemobile from '../../assets/donatemobile.svg';
+import mypagemobile from '../../assets/mypagemobile.svg';
+import votemobile from '../../assets/votemobile.svg';
 import Container from './Container';
 import Buttons from '../../components/Buttons';
 import classes from './HomePage.module.css';
@@ -42,20 +45,61 @@ const HomePage = () => {
       <section className={classes.description}>
         <div className={classes.descriptionSection}>
           <div className={classes.backgroundGradient}>
-            <div>후원하기</div>
-            <p>후원하기 설명 내용...</p>
+            <div className={classes.mobileLeft}>
+              <div className={`${classes.descriptionTitle} ${classes.r63}`}>
+                후원하기
+              </div>
+              <div className={classes.descriptionText}>
+                좋아하는 아이돌에게
+                <br />
+                쉽게 조공해보세요
+              </div>
+            </div>
+            <img
+              className={classes.mobile}
+              src={donatemobile}
+              alt="후원 이미지"
+            />
           </div>
         </div>
+
         <div className={classes.descriptionSection}>
           <div className={classes.backgroundGradient}>
-            <div>이달의 아티스트</div>
-            <p>이달의 아티스트 설명 내용...</p>
+            <div className={classes.mobileRight}>
+              <div className={`${classes.descriptionTitle} ${classes.l50}`}>
+                이달의 아티스트
+              </div>
+              <div className={classes.descriptionText}>
+                내 아티스트에게 1등의
+                <br />
+                영예를 선물하세요
+              </div>
+            </div>
+            <img
+              className={classes.mobile}
+              src={votemobile}
+              alt="투표 이미지"
+            />
           </div>
         </div>
+
         <div className={classes.descriptionSection}>
           <div className={classes.backgroundGradient}>
-            <div>나만의 아티스트</div>
-            <p>나만의 아티스트 설명 내용...</p>
+            <div className={classes.mobileLeft}>
+              <div className={`${classes.descriptionTitle} ${classes.r50}`}>
+                나만의 아티스트
+              </div>
+              <div className={classes.descriptionText}>
+                좋아하는 아티스트들의
+                <br />
+                소식을 모아보세요
+              </div>
+            </div>
+            <img
+              className={classes.mobile}
+              src={mypagemobile}
+              alt="마이페이지 이미지"
+            />
           </div>
         </div>
       </section>
