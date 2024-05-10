c8bb226ec49df7cf794043cc7fc91558968ae57f
yehaYoo <beauciel00@gmail.com>
2024-05-10 13:16:52 +0900

    버튼 컴포넌트 추가,폰트  반응형 적용

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,13 +1,14 @@
-import { useState } from "react";
-import { useDisclosure } from "@mantine/hooks";
-import { Button } from "@mantine/core";
+import { useState } from 'react';
+import { useDisclosure } from '@mantine/hooks';
+import { Button } from '@mantine/core';
+import ChartQueryExample from '../api/charts/ChartQueryExample';
 
-import Carousel from "../components/Carousel/Carousel";
-import ModalComponent from "../components/Modal/ModalComponent";
+import Carousel from '../components/Carousel/Carousel';
+import ModalComponent from '../components/Modal/ModalComponent';
 
 function List() {
   const [opened, { open, close }] = useDisclosure(false);
-  const [modalDataState, setModalDataState] = useState("donation");
+  const [modalDataState, setModalDataState] = useState('donation');
 
   const openModal = (modalName) => {
     open();
@@ -16,30 +17,31 @@ function List() {
 
   return (
     <div>
+      <ChartQueryExample />
       List
       <ModalComponent
         opened={opened}
         close={close}
         modalDataState={modalDataState}
       />
-      <div style={{ display: "flex", gap: "10px" }}>
+      <div style={{ display: 'flex', gap: '10px' }}>
         <Button
           onClick={() => {
-            openModal("creditWarn");
+            openModal('creditWarn');
           }}
         >
           크레딧 부족 모달
         </Button>
         <Button
           onClick={() => {
-            openModal("creditCharge");
+            openModal('creditCharge');
           }}
         >
           크레딧 충전 모달
         </Button>
         <Button
           onClick={() => {
-            openModal("donation");
+            openModal('donation');
           }}
         >
           후원하기 모달
