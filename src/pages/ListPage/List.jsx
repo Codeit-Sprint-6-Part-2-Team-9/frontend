10723eb713d06efafc4e2e898a10b7e44718fca5
doyo <83871696+doh-yo@users.noreply.github.com>
2024-05-11 22:32:34 +0900

    ✨ Feat: 리스트페이지 desktop 레이아웃 반영 (#31)
    * Fix: List페이지 중복 제거 및 경로 변경#31
    
    * Fix: ChartSection 스타일 수정#31
    
    * Fix: listPage desktop 레이아웃 반영 #31
    
    * Fix: 더보기 버튼 간결화

--- src/pages/ListPage/List.jsx
+++ src/pages/ListPage/List.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 import { useDisclosure } from '@mantine/hooks';
 import ModalComponent from '../../components/Modal/ModalComponent';
-import CarouselSection from "../../components/Carousel/Carousel";
+import CarouselSection from '../../components/Carousel/Carousel';
+import ChartSection from './ChartSection';
 import MyCreditRechargeButton from '../../components/MyCreditRechargeButton';
 import Typography from '../../components/Typography';
-import classes from "./ListPage.module.css";
+import classes from './ListPage.module.css';
 
 function List() {
   const [opened, { open, close }] = useDisclosure(false);
@@ -23,24 +24,22 @@ function List() {
         modalDataState={modalDataState}
       />
       <div className={classes.rechargeButtonwrapper}>
-      <div className={classes.creditRechargeButton}>
-      <MyCreditRechargeButton
-        onClick={() => {
-          openModal('creditCharge');
-        }}
-      />
-      </div>
+        <div className={classes.creditRechargeButton}>
+          <MyCreditRechargeButton
+            onClick={() => {
+              openModal('creditCharge');
+            }}
+          />
+        </div>
       </div>
       <div className={classes.typography}>
-      <Typography type='bold20lh26'>
-        후원을 기다리는 조공
-      </Typography>
+        <Typography type="bold20lh26">후원을 기다리는 조공</Typography>
       </div>
       <div className={classes.carouselSectionWrapper}>
-        <CarouselSection/>
+        <CarouselSection />
       </div>
       <div className={classes.chartSectionWrapper}>
-        < p className={classes.chartSection}></p>
+        <ChartSection />
       </div>
     </div>
   );
