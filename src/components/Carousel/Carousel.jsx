4a1f9549d2d34419bf3ac5e594d9ceeefcf7932c
yehaYoo <beauciel00@gmail.com>
2024-05-11 18:58:45 +0900

    refactor: 캐러셀 버튼 위치 설정 방식 변경(멘토님 리뷰 반영)#66

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,15 +1,17 @@
-import { useState, useEffect } from "react";
-import { Carousel } from "@mantine/carousel";
-import CarouselCard from "./CarouselCard";
-import getDonations from "../../api/donations/getDonations";
-import "@mantine/carousel/styles.css";
-import classes from "./Carousel.module.css";
-import prevIcon from "../../assets/btnArrowLeft.svg";
-import nextIcon from "../../assets/btnArrowRight.svg";
+import { useState, useEffect, useCallback } from 'react';
+import { Carousel } from '@mantine/carousel';
+import { ActionIcon, Box, Flex } from '@mantine/core';
+import CarouselCard from './CarouselCard';
+import getDonations from '../../api/donations/getDonations';
+import '@mantine/carousel/styles.css';
+import classes from './Carousel.module.css';
+import prevIcon from '../../assets/btnArrowLeft.svg';
+import nextIcon from '../../assets/btnArrowRight.svg';
 
 function CarouselSection() {
   const [cardData, setCardData] = useState([]);
   const [cursor, setCursor] = useState(null);
+  const [embla, setEmbla] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
@@ -17,30 +19,71 @@ function CarouselSection() {
         const data = await getDonations({ cursor });
         setCardData(data.list);
       } catch (error) {
-        console.error("Error fetching donations:", error);
+        console.error('Error fetching donations:', error);
       }
     }
 
     fetchData();
+  }, [cursor]);
 
-  }, []);
+  const handleNextBtn = useCallback(() => {
+    if (!embla) return;
+    const { scrollNext, canScrollNext } = embla;
+    if (!canScrollNext) return;
+    scrollNext();
+  }, [embla]);
+
+  const handlePrevBtn = useCallback(() => {
+    if (!embla) return;
+    const { scrollPrev, canScrollPrev } = embla;
+    if (!canScrollPrev) return;
+    scrollPrev();
+  }, [embla]);
 
   return (
-    <section className={classes.carouselWrapper}>
+    <Box component="section" className={classes.carouselWrapper} pos="relative">
+      <div className={classes.controlsWrapper}>
+        <Flex
+          justify="space-between"
+          pos={'relative'}
+          className={`${classes.flex}`}
+        >
+          <ActionIcon
+            w="40"
+            h="78px"
+            variant="transparent"
+            onClick={handlePrevBtn}
+          >
+            <img
+              src={prevIcon}
+              className={`${classes.control}`}
+              alt="캐러셀이전버튼"
+            />
+          </ActionIcon>
+          <ActionIcon
+            w="40"
+            h="78px"
+            variant="transparent"
+            onClick={handleNextBtn}
+          >
+            <img
+              src={nextIcon}
+              className={`${classes.control}`}
+              alt="캐러셀이후버튼"
+            />
+          </ActionIcon>
+        </Flex>
+      </div>
       <Carousel
         height={402}
-        slideSize={{ lg: "282", sm: "282", xs: "158" }}
-        slideGap={{ lg: "24", sm: "16", xs: "8" }}
+        slideSize={{ lg: '282', sm: '282', xs: '158' }}
+        slideGap={{ lg: '24', sm: '16', xs: '8' }}
         align="start"
         slidesToScroll={4}
-        previousControlIcon={
-          <img src={prevIcon} className={classes.carouselIcon} />
-        }
-        nextControlIcon={<img src={nextIcon} />}
-        classNames={{
-          root: classes.root,
-          controls: classes.controls,
-          control: classes.control,
+        withControls={false}
+        getEmblaApi={setEmbla}
+        styles={{
+          root: { maxWidth: 1200, margin: 'auto' },
         }}
       >
         {cardData.map((card) => (
@@ -49,7 +92,7 @@ function CarouselSection() {
           </Carousel.Slide>
         ))}
       </Carousel>
-    </section>
+    </Box>
   );
 }
 
