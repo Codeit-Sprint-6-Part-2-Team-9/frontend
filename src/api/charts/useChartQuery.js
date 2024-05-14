49b78d4fb035de5ac5cd37eae0828986ff3e685c
dotw0xff <154291841+dotw0xff@users.noreply.github.com>
2024-05-14 14:52:17 +0900

    🚑 Hotfix: ChartSection의 데이터 로딩시 발생하는 문제 해결(#31)

--- src/api/charts/useChartQuery.js
+++ src/api/charts/useChartQuery.js
@@ -1,15 +1,29 @@
-import { useInfiniteQuery } from '@tanstack/react-query';
+import { useInfiniteQuery, useQueryClient } from '@tanstack/react-query';
 import getCharts from './getCharts';
 import { reduceMultiPageDataWithListName, getNextPageParam } from '../utils';
 import { QUERY_KEYS } from '../config';
 
-const useChartQuery = (chartName = 'female', pageSizeCallback = () => 10) => useInfiniteQuery({
+const useChartQuery = (chartName = 'female', pageSizeCallback = () => 10) => {
+    const queryClient = useQueryClient();
+    const useChartQueryBase = useInfiniteQuery({
         queryKey: QUERY_KEYS[`${chartName}Chart`],
         queryFn: ({queryKey, pageParam: cursor}) => getCharts({ gender: queryKey[0], cursor }, pageSizeCallback),
         select: (multiPageData) => reduceMultiPageDataWithListName(multiPageData, 'idols'),
         initialPageParam: 0,
         getNextPageParam,
+        }
+    );
+
+    const refetchForDesktopSize = async (currentSize) => {
+        console.log(`called with ${currentSize}`)
+        const nextSize = (Math.ceil(currentSize / 10) * 10);
+        const data = await queryClient.fetchInfiniteQuery({
+            queryKey: QUERY_KEYS[`${chartName}Chart`],
+            queryFn: ({ queryKey }) => getCharts({ gender: queryKey[0], cursor: 0 }, () => nextSize),
+        });
+        return data;
     }
-);
+    return { ...useChartQueryBase, refetchForDesktopSize };
+}
 
 export default useChartQuery;
