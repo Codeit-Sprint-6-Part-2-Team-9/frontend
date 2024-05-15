c8a5931590e80d8a0d8ef5041948ca534183639a
dotw0xff <154291841+dotw0xff@users.noreply.github.com>
2024-05-15 12:33:17 +0900

    🚑 Fix: 메인 페이지 이외의 페이지들이 제대로 출력되지 않던 문제 해결 (#39)
    * ✨ feat: CI/CD 구현을 위한 개발, 배포용 설정 분리(#39)
    
    * 📦 Chore: 서버URL, 팀 이름을 소스코드에서 분리함(#39).
    
    * 🚑 Hotfix: vite 설정의 오기로 인하여 baseurl이 올바르게 지정되지 않는 문제 수정(#39)
    
    * 🚑 Hotfix: 메인 페이지 이외의 페이지들이 제대로 출력되지 않던 문제 해결(#39)

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { createHtmlPlugin } from 'vite-plugin-html';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
 
 export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd());
@@ -12,11 +14,19 @@ export default ({ mode }) => {
         minify: true,
         inject: {
           data: {
-            VITE_APP_URL: env.VITE_APP_URL,
+            VITE_APP_URL : env.VITE_APP_URL,
           },
         },
+      }),
+      viteStaticCopy({
+        targets: [
+          {
+            src: 'public/404.html',
+            dest: '',
+          },
+        ],
       }),
     ],
     base: env.VITE_BASE_URL,
   });
-}
+};
